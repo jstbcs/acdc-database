b2894606316d8641e3d4b793f3c216e0dd5399fe
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2023-11-30 14:43:32 +0100

    join tables for new structure

--- reading_list_objects_to_db.R
+++ reading_list_objects_to_db.R
@@ -17,6 +17,21 @@ for (i in seq_along(list_files)){
 
 names(lists) = paste0("publication", seq_along(list_files))
 
+# Need to join the row from between_table to the appropriate datasets "dataset" table 
+# also within each dataset, join the columns from condition_table to the within_table
+for (ipub in seq_along(lists)){
+  for (istudy in 2:length(lists[[ipub]])){
+    between_table = lists[[ipub]][[istudy]]$between_table
+    for (idata in 3:length(lists[[ipub]][[istudy]])){
+      data_num = idata - 2
+      between_info = between_table[data_num, -1]
+      lists[[ipub]][[istudy]][[idata]]$dataset_table = cbind(lists[[ipub]][[istudy]][[idata]]$dataset_table, between_info)
+      
+      condition_info = lists[[ipub]][[istudy]][[idata]]$condition_table
+      lists[[ipub]][[istudy]][[idata]]$within_table = cbind(lists[[ipub]][[istudy]][[idata]]$within_table, condition_info)
+    }
+  }
+}
 
 # Adding lists ------
 
