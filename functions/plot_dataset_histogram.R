32872742e791d0b618c88b3c2ca08c2ff745e436
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2023-12-21 09:34:08 +0100

    split descriptive plots

--- functions/plot_dataset_histogram.R
+++ functions/plot_dataset_histogram.R
@@ -1,5 +1,23 @@
 plot_dataset_histograms <- function(dataset, order_by = "Dataset ID"){
-  dataset |> 
+  acc_plot <- dataset |> 
+    dplyr::mutate(dataset_id = factor(`Dataset ID`)) |>
+    ggplot2::ggplot(
+      mapping = ggplot2::aes(
+        x = forcats::fct_reorder(dataset_id, !!sym(paste(rlang::get_expr(rlang::enquo(order_by)))), .desc = TRUE),
+        y = `Mean accuracy (dataset)`
+      )
+    ) +
+    ggplot2::geom_point(
+      size = 3
+    ) +
+    ggplot2::labs(
+      title = "Mean Accuracy",
+      x = "dataset_id",
+      caption = "Values are based on the 'mean_dataset_acc' column."
+    )+
+    ggplot2::theme_classic()
+  
+  rt_plot <- dataset |> 
     dplyr::mutate(dataset_id = factor(`Dataset ID`)) |>
     ggplot2::ggplot(
       mapping = ggplot2::aes(
@@ -10,26 +28,13 @@ plot_dataset_histograms <- function(dataset, order_by = "Dataset ID"){
     ggplot2::geom_point(
       size = 3
     ) +
-    ggplot2::geom_bar(
-      mapping = ggplot2::aes(
-        y = `Mean accuracy (dataset)`
-      ),
-      color = "black",
-      stat = "identity",
-      fill = "transparent",
-      width = 0.6
-    )+
-    ggplot2::scale_y_continuous(
-      name = "Response Time (in s)",
-      sec.axis = ggplot2::sec_axis(~., name = "Accuracy"),
-      breaks = c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1),
-      labels = c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
-    ) +
     ggplot2::labs(
-      title = "Mean Response Time and Accuracy",
+      title = "Mean Response Time (in s)",
       x = "dataset_id",
-      caption = "Dots represent response time, bars indicate accuracy.\nValues are based on the 'mean_dataset_rt' and 'mean_dataset_acc' column."
+      caption = "Values are based on the 'mean_dataset_rt' column."
     )+
     ggplot2::theme_classic()
+  
+  ggpubr::ggarrange(acc_plot, rt_plot, ncol = 1, nrow = 2)
 }
 
