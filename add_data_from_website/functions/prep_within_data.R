5a98c81bbf58a9097c0fa652e7592262e717bd20
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2025-05-07 17:05:22 +0200

    prep functions done

--- add_data_from_website/functions/prep_within_data.R
+++ add_data_from_website/functions/prep_within_data.R
@@ -1,17 +1,28 @@
 prep_within_data <- function(within_data, raw_data){  
   within_data = clean_char_columns(within_data)
 
-  df_test = remove_practice(raw_data) # remove practice trials
-
-  added_info = data.frame(
-    percentage_congruent = get_perc_congr(df_test), 
-    percentage_neutral = get_perc_neut(df_test), 
-    n_obs = get_n_obs(df_test),
-    mean_obs_per_participant = get_mean_obs_pp(df_test), 
-    mean_condition_rt = get_mean_rt(df_test),
-    mean_condition_acc = get_mean_acc(df_test)
-  )
-
+  # Remove practice trials
+  df_test <- remove_practice(raw_data)
+  
+  # Split by condition and compute summary statistics
+  added_info = data.frame()
+  
+  for (i in 1:nrow(within_data)){
+    df_cond = df_test[df_test$within == within_data[i, "identifier"], ]
+    
+    added_info = rbind(added_info,
+                       data.frame(
+                         percentage_congruent = get_perc_congr(df_cond),
+                         percentage_neutral = get_perc_neut(df_cond),
+                         n_obs = get_n_obs(df_cond),
+                         mean_obs_per_participant = get_mean_obs_pp(df_cond),
+                         mean_condition_rt = get_mean_rt(df_cond),
+                         mean_condition_acc = get_mean_acc(df_cond)
+                       )
+    )
+  }
+  
+  
   within_data = cbind(within_data, added_info)
 
   return(within_data)
