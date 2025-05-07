40635942b64725f5e9bd8f5c6a9ce12998a6ea19
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2025-05-07 11:53:36 +0200

    write extraction function

--- add_data_from_website/functions/extract_from_submission_json.R
+++ add_data_from_website/functions/extract_from_submission_json.R
@@ -1,77 +1,66 @@
 extract_from_submission_json <- function(json_path){
   json_obj = jsonlite::read_json(json_path)
   
-  json_obj = replace_999_with_na(json_obj)
+  # json_obj = replace_999_with_na(json_obj)
   
   submission_list = list()
   
-  submission_list$publication_data = as.data.frame(json_obj$publication_data)
+  submission_list$publication_table = as.data.frame(json_obj$publication_data)
   
-  n_statementsets = length(json_obj$statementset_info)
+  n_tasks = length(json_obj$task_info)
   
-  if (n_statementsets > 0) {
-    if (length(json_obj$statementset_info[[1]]) > 0){
-      submission_list$statementset_info = vector(mode = "list", n_statementsets)
-      for (istatementset in 1:n_statementsets){
-        submission_list$statementset_info[[istatementset]]$publication = json_obj$statementset_info[[istatementset]]$statementset_publication
-        
-        if (trimws(submission_list$statementset_info[[istatementset]]$publication) == ""){
-          submission_list$statementset_info[[istatementset]]$publication = submission_list$publication_data$apa_reference
-        }
-        
-        submission_list$statementset_info[[istatementset]]$statementset_data = as.data.frame(data.table::rbindlist(json_obj$statementset_info[[istatementset]]$statementset_data))
-        
-        columns_to_extract = c("statement_identifier", "statement_text", "statement_accuracy", "statement_category", "proportion_true")
-        submission_list$statementset_info[[istatementset]]$statementset_data = dplyr::select(submission_list$statementset_info[[istatementset]]$statementset_data, dplyr::any_of(columns_to_extract))
+  if (n_tasks > 0) {
+    if (length(json_obj$task_info[[1]]) > 0){
+      submission_list$task_info = vector(mode = "list", n_tasks)
+      for (itask in 1:n_tasks){
+        submission_list$task_info[[itask]]$task_data = as.data.frame(json_obj$task_info[[itask]]$task_info$task_data)
       }
     }
   } else {
-    n_statementsets = 0
+    n_tasks = 0
   }
   
   n_studies = length(json_obj$study_info)
   
-  submission_list$study_info = vector(mode = "list", length = n_studies)
   for (istudy in 1:n_studies){
-    submission_list$study_info[[istudy]]$study_data = as.data.frame(json_obj$study_info[[istudy]]$study_data)
-    submission_list$study_info[[istudy]]$repetition_data = as.data.frame(data.table::rbindlist(json_obj$study_info[[istudy]]$repetition_data))
-
-    columns_to_extract = c("subject", "presentation_identifier", "trial", "response", "repeated")
-    
-    # Here check if has conditions
-    if (json_obj$study_info[[istudy]]$condition_data$has_within_conditions == "1"){
-      submission_list$study_info[[istudy]]$within_data = as.data.frame(data.table::rbindlist(json_obj$study_info[[istudy]]$condition_data$within_condition_details))
-      columns_to_extract = c(columns_to_extract, "within_identifier")
-    }
-    
-    if (json_obj$study_info[[istudy]]$condition_data$has_between_conditions == "1"){
-      submission_list$study_info[[istudy]]$between_data = as.data.frame(data.table::rbindlist(json_obj$study_info[[istudy]]$condition_data$between_condition_details))
-      columns_to_extract = c(columns_to_extract, "between_identifier")
-    }
-    
-    # Here extract only necessary columns from the raw data
-    submission_list$study_info[[istudy]]$raw_data = as.data.frame(data.table::rbindlist(json_obj$study_info[[istudy]]$raw_data))
-    
-    # Add columns to extract
-    if (json_obj$study_info[[istudy]]$study_data$statementset_name != "no information"){
-      columns_to_extract = c(columns_to_extract, "statement_identifier")
-    }
-    
-    if (json_obj$study_info[[istudy]]$study_data$rt_measured == "1"){
-      columns_to_extract = c(columns_to_extract, "rt")
-    }
-    
-    if (json_obj$study_info[[istudy]]$study_data$subjective_certainty == "1"){
-      columns_to_extract = c(columns_to_extract, "certainty")
-    }
-    
-    submission_list$study_info[[istudy]]$raw_data = as.data.frame(submission_list$study_info[[istudy]]$raw_data)[, columns_to_extract]
-    
+    study_info = list()
+  
+    study_info$study_table = as.data.frame(json_obj$study_info[[istudy]]$study_data)
+   
     # Deal with submitted additional measures
     if (json_obj$study_info[[istudy]]$measurement_data$additional_measures == "1"){
-      submission_list$study_info[[istudy]]$measurement_data = as.data.frame(data.table::rbindlist(json_obj$study_info[[istudy]]$measurement_data$measures))
+      study_info$measurement_table= as.data.frame(data.table::rbindlist(json_obj$study_info[[istudy]]$measurement_data$measures))
     }
+    
+    n_datasets = length(json_obj$study_info[[istudy]]$dataset_info)
+    
+    for (idata in 1:n_datasets){
+      data_info = list()
+      
+      # Extract the task name
+      task_name_used = json_obj$study_info[[istudy]]$dataset_info[[idata]]$dataset_data$task_name
+      
+      # Retrieve ID number
+      task_id_used = as.numeric(grep("\\d+$", task_name_used))
+      
+      data_info$task_table = submission_list$task_info[[task_id_used]]$task_data
+      
+      data_info$dataset_table = as.data.frame(json_obj$study_info[[istudy]]$dataset_info[[idata]]$dataset_data)
+      
+      if (json_obj$study_info[[istudy]]$dataset_info[[idata]]$within_data$has_within_conditions == "1"){
+        data_info$within_table = as.data.frame(data.table::rbindlist(json_obj$study_info[[istudy]]$dataset_info[[idata]]$within_data$within_condition_details))
+      }
+      
+      data_info$observation_table = as.data.frame(data.table::rbindlist(json_obj$study_info[[istudy]]$dataset_info[[idata]]$raw_data))
+      
+      study_info[[paste0("data_", idata)]] = data_info
+    }
+    
+    submission_list[[paste0("study_", istudy)]] = study_info
   }
   
-  return(submission_list)
+  object = list()
+  object$publication_1= submission_list
+  
+  return(object)
 }
