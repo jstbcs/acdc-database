a6f4b1edba17855e922fb888daa63a8a5bef139d
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2026-04-08 17:12:21 +0200

    Example data submission script

--- add_data_from_website/add_data_from_website.R
+++ add_data_from_website/add_data_from_website.R
@@ -17,27 +17,28 @@ library(DBI)
 
 # Prepare Submission
 # Read from JSON
+# Loop over this if you want to submit multiple files
 json_path <- "./add_data_from_website/test/submission_Rey-Mermet_2018.json"
-
 
 unprepped_obj <- extract_from_submission_json(json_path)
 # Transform into the usual object thing
 prepped_obj <- prep_submission_data(unprepped_obj)
 
-# Allow inspection
-inspect_publication_data(prepped_obj)
-inspect_study_data(prepped_obj)
-inspect_task_data(prepped_obj)
-inspect_measurement_data(prepped_obj)
-inspect_dataset_data(prepped_obj)
-inspect_within_data(prepped_obj)
-inspect_raw_data(prepped_obj)
+# # Allow inspection
+# inspect_publication_data(prepped_obj)
+# inspect_study_data(prepped_obj)
+# inspect_task_data(prepped_obj)
+# inspect_measurement_data(prepped_obj)
+# inspect_dataset_data(prepped_obj)
+# inspect_within_data(prepped_obj)
+# inspect_raw_data(prepped_obj)
 
 # Check structure with functions
 check_overall_structure(prepped_obj)
+# MAKE SURE THIS PASSES WITHOUT ERRORS
 
 # Submit to db
-path = "./acdc_website4.db"
+path = "./acdc.db"
 create_empty_db(path)
 
 db_conn = DBI::dbConnect(RSQLite::SQLite(), path)
@@ -46,15 +47,15 @@ add_object(db_conn, prepped_obj)
 
 DBI::dbDisconnect(db_conn)
 
-# Check
-library(acdcquery)
-con <- connect_to_db(path)
-arguments <- list() %>% 
-  add_argument(
-    con,
-    "study_id",
-    "equal",
-    "1"
-  )
-
-result <- query_db(con, arguments, "default", "dataset_table")
+# # Check
+# library(acdcquery)
+# con <- connect_to_db(path)
+# arguments <- list() %>% 
+#   add_argument(
+#     con,
+#     "study_id",
+#     "equal",
+#     "1"
+#   )
+# 
+# result <- query_db(con, arguments, "default", "within_table")
