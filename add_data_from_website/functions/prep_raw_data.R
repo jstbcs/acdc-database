f90c12535f678a0c059146afdedb72c3aab6aec2
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2026-04-08 17:03:08 +0200

    clean prep raw data to only include within naming

--- add_data_from_website/functions/prep_raw_data.R
+++ add_data_from_website/functions/prep_raw_data.R
@@ -7,6 +7,10 @@ prep_raw_data <- function(raw_data){
   raw_data[raw_data == TRUE] = 1
   raw_data[raw_data == FALSE] = 0
   
+  raw_data = dplyr::select(raw_data, dplyr::all_of(
+    c("subject","block", "trial",	"within_identifier",
+      "congruency",	"accuracy",	"rt")))
+  
   clean_raw_data = clean_char_columns(raw_data)
 
   clean_raw_data = raw_data |>
@@ -19,6 +23,12 @@ prep_raw_data <- function(raw_data){
     
     clean_raw_data$rt = ifelse(clean_raw_data$rt > 30, NA, clean_raw_data$rt)
   }
+  
+  # To make sure it is also character, like within_data might sometimes contain
+  clean_raw_data$within_identifier = as.character(clean_raw_data$within_identifier)
+  
+  # Some functions need one, some the other
+  clean_raw_data$within = as.character(clean_raw_data$within_identifier)
   
   return(clean_raw_data)
 }
